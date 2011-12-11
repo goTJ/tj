d9b40eb9fe5678bfbad5b770095e9fad3bb31204
TJ <tienjung@gmail.com>
2011-12-12 01:23:06 +0800

    finish ex8

--- sf_ml/mlclass-ex8/cofiCostFunc.m
+++ sf_ml/mlclass-ex8/cofiCostFunc.m
@@ -12,10 +12,10 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
             
+J = sum(sum(((X * Theta' - Y) .^ 2).* R)) / 2 + (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2))) * lambda / 2;
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda * Theta;
 % You need to return the following values correctly
-J = 0;
-X_grad = zeros(size(X));
-Theta_grad = zeros(size(Theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost function and gradient for collaborative
